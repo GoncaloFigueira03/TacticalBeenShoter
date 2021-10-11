c5cb35bd0e0c2485c325960770a841f0c9ef4327
Ec1ipse03 <goncalogamer.pt2804@gmail.com>
2021-10-11 21:36:39 +0100

    bsdgsrgser

---
+++ Source/TacticalBeenShoter/Public/MainMenuGameBase.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainMenuGameBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TACTICALBEENSHOTER_API AMainMenuGameBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
